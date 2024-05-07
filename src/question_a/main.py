12da64faef0b94acd108b7820dac1fb12eacb48c
Jaymes Lightfoot <157078893+ToriALaMode@users.noreply.github.com>
2024-05-07 18:25:38 -0500

    Update main.py

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,15 @@
 #add import
+from src.qustion_a import get_numbers
+from src.qustion_a import get_highest
+from src.qustion_a import get_total
+from src.qustion_a import get_average
+
+def main():
+    numbers = get_numbers()
+    print("Lowest number:", get_lowest(numbers))
+    print("Highest number:", get_highest(numbers))
+    print("Total:", get_total(numbers))
+    print("Average:", get_average(numbers))
+
+if __name__ == "__main__":
+    main()
