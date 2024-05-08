eb9ee5c04effff1cd605789189756e448c49cf5b
Jaymes Lightfoot <157078893+ToriALaMode@users.noreply.github.com>
2024-05-08 02:13:04 +0000

    Question A

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,11 @@
 #add import
+from question_a import get_average, get_highest, get_lowest, get_numbers, get_total
+def main():
+    numbers = get_numbers()
+    print("Lowest number:", get_lowest(numbers))
+    print("Highest number:", get_highest(numbers))
+    print("Total:", get_total(numbers))
+    print("Average:", get_average(numbers))
+
+if __name__ == "__main__":
+    main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -7,3 +7,21 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+def get_numbers():
+    numbers = []
+    for _ in range(5):
+        num = float(input("Enter a number: "))
+        numbers.append(num)
+    return numbers
+    
+def get_lowest(numbers): # The lowest number in the list
+    return min(numbers)
+    
+def get_highest(numbers): # The highest number in the list
+    return max(numbers)
+
+def get_total(numbers): # The total of the numbers in the list
+    return sum(numbers)
+
+def get_average(numbers): # The average of the numbers in the list
+    return sum(numbers) / len(numbers)
