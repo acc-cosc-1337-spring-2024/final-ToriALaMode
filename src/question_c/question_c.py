9dc25c1bb66301f2850a007d6db3911687ae5db0
Jaymes Lightfoot <157078893+ToriALaMode@users.noreply.github.com>
2024-05-07 20:18:48 -0500

    Update question_c.py

--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -8,8 +8,16 @@
 # Implementation:
 # Create a Stock class with one constructor with symbol and company_name parameters, private attributes(variables) symbol and company_name, and public functions get_symbol 
 # and get_company_name.
-
-# Tests:
-# No Test cases.
-
 # write functions here, don't add input('') statements here!
+
+class Stock:
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+
+    def get_company_name(self):
+        return self.__company_name
+
